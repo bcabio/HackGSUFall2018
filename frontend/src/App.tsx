073ba4daa4af51c5c31f5a70f912cfa5b24c3bf6
Flaviu Tamas <tamasflaviu@gmail.com>
2018-10-06 02:26:12 -0400

    Use react-router for routing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,47 +2,61 @@ import * as React from 'react';
 
 import * as moment from 'moment';
 import { Grid, Nav, Navbar, NavItem } from 'react-bootstrap';
+import { LinkContainer } from 'react-router-bootstrap';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
+import Home from './components/Home';
 import TransactionList from './components/TransactionList';
+
+const ExampleTransactionList = () => (
+  <TransactionList
+    transactions={[
+      {
+        items: [{ name: 'old apple', price: 500 }],
+        time: moment().subtract(7, 'days'),
+        user: 'flaviu'
+      },
+      {
+        items: [{ name: 'old apple', price: 500 }],
+        time: moment().subtract(7, 'days'),
+        user: 'flaviu'
+      },
+      {
+        items: [{ name: 'old apple', price: 500 }],
+        time: moment().subtract(7, 'days'),
+        user: 'flaviu'
+      }
+    ]}
+  />
+);
 
 class App extends React.Component {
   public render() {
     return (
-      <React.Fragment>
-        <Navbar>
-          <Navbar.Header>
-            <Navbar.Brand>
-              <a href="#home">React-Bootstrap</a>
-            </Navbar.Brand>
-          </Navbar.Header>
-          <Nav>
-            <NavItem eventKey={1} href="#">
-              Link
-            </NavItem>
-          </Nav>
-        </Navbar>
-        <Grid>
-          <TransactionList
-            transactions={[
-              {
-                items: [{ name: 'old apple', price: 500 }],
-                time: moment().subtract(7, 'days'),
-                user: 'flaviu'
-              },
-              {
-                items: [{ name: 'old apple', price: 500 }],
-                time: moment().subtract(7, 'days'),
-                user: 'flaviu'
-              },
-              {
-                items: [{ name: 'old apple', price: 500 }],
-                time: moment().subtract(7, 'days'),
-                user: 'flaviu'
-              }
-            ]}
-          />
-        </Grid>
-      </React.Fragment>
+      <Router>
+        <React.Fragment>
+          <Navbar>
+            <Navbar.Header>
+              <Navbar.Brand>Apple Store</Navbar.Brand>
+            </Navbar.Header>
+            <Nav>
+              <LinkContainer to="/transactions">
+                <NavItem eventKey={1} href="/transactions">
+                  Transactions
+                </NavItem>
+              </LinkContainer>
+            </Nav>
+          </Navbar>
+          <Grid>
+            <Route exact path="/" component={Home} />
+            <Route
+              exact
+              path="/transactions"
+              component={ExampleTransactionList}
+            />
+          </Grid>
+        </React.Fragment>
+      </Router>
     );
   }
 }
---
+++ frontend/src/components/Home.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import { Col, Row } from 'react-bootstrap';
+
+const Home: React.SFC<undefined> = () => {
+  return (
+    <Row>
+      <Col>Welcome!</Col>
+    </Row>
+  );
+};
+
+export default Home;
